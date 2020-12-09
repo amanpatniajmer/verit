cc9ab56f953691e41a9a186ad0ba4100d3e20d96
baiju-bawra <shreyash.baijal.cer17@itbhu.ac.in>
2020-12-09 15:45:40 +0530

    Ainvayi

--- src/components/pages/admin/AddEvent.js
+++ src/components/pages/admin/AddEvent.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, {useState} from 'react';
+import AddInternal from "./AddInternal";
+import AddExternal from "./AddExternal";
 import cultLogo from "../../../img/small-logo.jpg"
 
 const AddEvent = () => {
-    const a=(
-        <div  className="add-event">
+    const [click, setClick] = useState("");
+    const a = (
+        <div className="add-event">
             <img src={cultLogo} alt="" style={{ height: "120px", width: "120px" }} />
             <h1 className="large text-primary text-center">Cultural Council: General Secretary</h1>
             <h1 className="text-center" style={{ fontFamily: "Arial, Helvetica, sans-serif", margin: "20px 0px"}}>Add an event:{" "}
-                <Link to ="../admin/addinternal"><button className="btn-danger btn">Internal Event</button></Link>
-                <Link to ="../admin/addexternal"><button className="btn-dark btn">External Event</button></Link>
-                </h1>
+            </h1>
+            <div>
+                <button className="event-btn" onClick={() => setClick("Internal")}>Internal <span>Event</span></button>
+
+                <button className="event-btn" onClick={() => setClick("External")}>External <span>Event</span></button>
+            </div>
         </div>
     )
-    return a;
+    if(click === "Internal")
+        return <AddInternal click={setClick}/>
+    else if(click === "External")
+        return <AddExternal click={setClick}/>
+    else return a;
 }
 
 export default AddEvent;
