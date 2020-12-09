82b07b80a4018b8e27fbd9c574f6027255d218c8
amanpatniajmer <amanpatniajmer@gmail.com>
2020-12-09 18:04:33 +0530

    ..

--- src/components/pages/admin/AddEvent.js
+++ src/components/pages/admin/AddEvent.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom'
 import AddInternal from "./AddInternal";
 import AddExternal from "./AddExternal";
 import cultLogo from "../../../img/small-logo.jpg"
